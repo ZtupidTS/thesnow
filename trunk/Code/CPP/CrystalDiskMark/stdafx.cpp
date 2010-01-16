b2d1946847bc358db70e6f0c9cb5f001bb239dfb
autoitv3 <autoitv3@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2010-01-16 18:14:35 +0000

    add CrystalDiskMark

---
+++ trunk/Code/CPP/CrystalDiskMark/stdafx.cpp
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------*/
+//       Author : hiyohiyo
+//         Mail : hiyohiyo@crystalmark.info
+//          Web : http://crystalmark.info/
+//      License : The modified BSD license
+//
+//                                Copyright 2007 hiyohiyo, All rights reserved.
+/*---------------------------------------------------------------------------*/
+
+#include "stdafx.h"
+
+
