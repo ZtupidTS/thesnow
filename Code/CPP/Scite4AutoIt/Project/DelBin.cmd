d93803c6151fa421436baff22274812aaca475c4
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2010-06-09 17:56:09 +0000

    SciTe update(支持X64)

---
+++ Code/CPP/Scite4AutoIt/Project/DelBin.cmd
@@ -0,0 +1,2 @@
+del /S /Q *.a *.aps *.bsc *.dll *.dsw *.exe *.idb *.ilc *.ild *.ilf *.ilk *.ils *.lib *.map *.ncb *.obj *.o *.opt *.pdb *.plg *.res *.sbr *.tds *.exp *.user BuildLog.* *.dep *.manifest>NUL:
+rd /s /q Bin
