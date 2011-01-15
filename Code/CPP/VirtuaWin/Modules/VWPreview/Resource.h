dfd0706e917e1d43e54f1fd9a5e1a6f30ec63dc9
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2011-01-15 15:49:59 +0000

    VirtuaWin update

---
+++ Code/CPP/VirtuaWin/Modules/VWPreview/Resource.h
@@ -0,0 +1,24 @@
+
+#define IDC_STATIC              -1
+#define	IDD_MAINDIALOG   	100
+#define IDI_VIRTUAWIN           101
+#define	ID_OK			102
+#define	ID_APPLY		103
+#define	IDC_WN_HOTKEY_ENT	1001
+#define	IDC_WN_HOTKEY_WIN	1002
+#define	IDC_WN_VWCONTROL	1003
+#define	IDC_WN_OPTIMLAY		1004
+#define	IDC_WN_CLOSE    	1005
+#define	IDC_WN_IMGHEIGHT	1006
+#define	IDC_FS_HOTKEY_ENT	1007
+#define	IDC_FS_HOTKEY_WIN	1008
+#define	IDC_FS_VWCONTROL	1009
+#define	IDC_FS_OPTIMLAY		1010
+#define	IDC_FS_ZOOM     	1011
+#define	IDC_FS_UPDATE     	1012
+#define	IDC_FS_UDCONT     	1013
+#define	IDC_TE_ENABLE    	1014
+#define	IDC_TE_EFFECT     	1015
+#define	IDC_GS_FULLSIZE     	1016
+#define	IDC_GS_KEEPIMGS     	1017
+
