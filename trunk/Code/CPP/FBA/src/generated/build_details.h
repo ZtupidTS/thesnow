63eb8559755132ff9ef1d17e5053ed43e2d0a960
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2011-08-26 10:47:13 +0000

    add FBA

---
+++ trunk/Code/CPP/FBA/src/generated/build_details.h
@@ -0,0 +1,8 @@
+#define BUILD_TIME 12:32:40
+#define BUILD_DATE Jun 24 2010
+#define BUILD_CHAR Unicode
+#define BUILD_CPU  i686
+#define BUILD_COMP Visual C++ 9.0
+#ifndef _MSC_VER
+ #define _MSC_VER  1500
+#endif
