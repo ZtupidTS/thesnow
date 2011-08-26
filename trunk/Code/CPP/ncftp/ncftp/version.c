540715a3a3f42af4ca617a703bf3bd16dc56b4ed
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2011-08-26 11:40:55 +0000

    add ncftp

---
+++ trunk/Code/CPP/ncftp/ncftp/version.c
@@ -0,0 +1,30 @@
+#include "syshdrs.h"
+#ifdef PRAGMA_HDRSTOP
+#	pragma no_pch
+#	pragma hdrstop
+#endif
+
+/******************************************************************************/
+
+const char gVersion[] = "@(#) NcFTP 3.2.5/471 Jan 17 2011, 09:52 PM";
+
+/******************************************************************************/
+
+#ifdef O_S
+const char gOS[] = O_S;
+#elif (defined(WIN32) || defined(_WINDOWS)) && !defined(__CYGWIN__)
+const char gOS[] = "Windows";
+#elif defined(__CYGWIN__)
+const char gOS[] = "Cygwin";
+#else
+const char gOS[] = "UNIX";
+#endif
+
+/******************************************************************************/
+
+const char gCopyright[] = "@(#) \
+Copyright (c) 1992-2011 by Mike Gleason.\n\
+All rights reserved.\n\
+";
+
+/******************************************************************************/
