2b0bfe76ce9eefcf8655e46a8fa36da3c2e29e3b
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2011-08-26 10:13:44 +0000

    add ClassicShell

---
+++ Code/CPP/ClassicShell/ClassicStartMenu/ClassicStartMenuDLL/SettingsUI.h
@@ -0,0 +1,8 @@
+// Classic Shell (c) 2009-2011, Ivo Beltchev
+// The sources for Classic Shell are distributed under the MIT open source license
+
+#pragma once
+
+void InitSettings( void );
+STARTMENUAPI void EditSettings( bool bModal );
+STARTMENUAPI void LogHookError( int error );
