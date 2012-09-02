2a5f8b35e6dce4165dba867193fffd075dbe894c
thegfw <thegfw@58104c5a-2bea-11de-ae9b-2be1a451ffb1>
2012-09-02 17:14:30 +0000

    SciTe updated

--- Code/CPP/Scite4AutoIt/Project/DelBin.cmd
+++ Code/CPP/Scite4AutoIt/Project/DelBin.cmd
@@ -1,2 +1,3 @@
 del /S /Q *.a *.aps *.bsc *.dll *.dsw *.exe *.idb *.ilc *.ild *.ilf *.ilk *.ils *.lib *.map *.ncb *.obj *.o *.opt *.pdb *.plg *.res *.sbr *.sdf *.tds *.exp *.user BuildLog.* *.dep *.manifest>NUL:
 rd /s /q Bin
+md bin
